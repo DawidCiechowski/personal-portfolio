3fe9a3d7351fc5b664f7e9ecc0c930355bcef7f1
Dawid Ciechowski <dawid.ciechowski@gmail.com>
2024-10-15 18:25:53 +0200

    :bug: Bugfix. Missing space in Facts style

--- src/components/Facts.tsx
+++ src/components/Facts.tsx
@@ -10,7 +10,7 @@ interface FactsProps {
 
 const Fact: React.FC<FactsProps> = ({ value, children }) => {
   return (
-    <div className="tw-flex tw-items-center tw-gap-1 tw-text-[1.1rem]lg:tw-text-[1.7rem] tw-font-semibold tw-text-main-color">
+    <div className="tw-flex tw-items-center tw-gap-1 tw-text-[1.1rem] md:tw-text-[1.7rem] tw-font-semibold tw-text-main-color">
       <CountUp end={value} duration={3} delay={2} className="tw-border-b-3px tw-border-solid tw-text-[4rem] lg:tw-text-[5rem] tw-font-bold tw-pr-4 tw-pt-[-2rem]" />
       {children}
     </div>
