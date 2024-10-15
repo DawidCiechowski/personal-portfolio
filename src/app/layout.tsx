89072ae59926529e49cc0c4fef58fd2dd24a3100
Dawid Ciechowski <dawid.ciechowski@gmail.com>
2024-10-15 19:00:33 +0200

    Still don't know what's happening with devicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -37,7 +37,12 @@ export default function RootLayout({
           href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
           rel="stylesheet"
         ></link>
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"></link>
+
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css"
+        />
       </head>
       <body className={`${oswaldSans.variable} ${pixelifySans.variable}`}>
         <Header />
