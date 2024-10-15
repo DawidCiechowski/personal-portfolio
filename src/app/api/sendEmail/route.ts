4106cbc533febd1dcf5f6bce15d93a0704a9d494
Dawid Ciechowski <dawid.ciechowski@gmail.com>
2024-10-15 15:40:43 +0200

    Updated style for email response

--- src/app/api/sendEmail/route.ts
+++ src/app/api/sendEmail/route.ts
@@ -25,7 +25,7 @@ export async function POST(req: NextRequest) {
 
   const mailToSelfOptions = {
     from: process.env.SMTP_USER,
-    to: process.env.SMTP_USER, // Send the email to yourself
+    to: process.env.SMTP_USER,
     subject: `New Contact Form Submission From: ${name}`,
     html: `
       <div style="font-family: Arial, sans-serif;">
@@ -42,24 +42,31 @@ export async function POST(req: NextRequest) {
     to: email,
     subject: `Your Message Arrived Safely! :)`,
     html: `
-      <div style="border-radius: 0.375rem; font-family: Arial, sans-serif; padding: 20px; max-width: 600px; margin: auto; border: 1px solid #f0f0f0; background-color: #FF6500; color: #1E3E62">
-        <h1 style="color: #000000; font-weight: 800; text-align: center;">Message Received</h1>
-        <p style="font-size: 16px; font-weight: 500;  color: #1E3E62;">Hi ${name}! :D</p>
-        <p style="font-size: 16px; font-weight: 500; color: #1E3E62;">Thank you for reaching out! Your message has arrived to me safely and I'll try to answer ASAP.</p>
-        <p style="font-size: 16px; font-weight: 500;  color: #1E3E62;">Here is a copy of your message:</p>
-        <div style="border-left: 4px solid #0B192C; padding-left: 15px; margin-top: 15px;">
-          <p style="font-size: 16px; font-weight: 500; color: #1E3E62;">${message}</p>
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap');
+        body {
+          font-family: "Oswald", sans-serif;
+        }
+      </style>
+      <div style="border-radius: 0.375rem; font-family: 'Oswald', sans-serif; padding: 20px; max-width: 600px; margin: auto; border: 1px solid #f0f0f0; background-color: #000000; color: #1E3E62">
+        <h1 style="color: #FF6500; font-weight: 700; text-align: center;">Message Received</h1>
+        <p style="font-size: 16px; font-weight: 500;  color: #FF6500;">Hi ${name}! :D</p>
+        <p style="font-size: 16px; font-weight: 500; color: #FF6500;">Thank you for reaching out! Your message has arrived to me safely and I'll try to answer ASAP.</p>
+        <p style="font-style: italic; font-size: 16px; color: #FF6500;">This is an automatic message. Please do not respond to it.</p>
+        <p style="font-size: 16px; font-weight: 500;  color: #FF6500;">Here is a copy of your message:</p>
+        <div style="border-left: 4px solid #FF6500; padding-left: 15px; margin-top: 15px;">
+          <p style="font-size: 16px; font-weight: 500; color: #FF6500;">${message}</p>
         </div>
-        <p style="font-size: 16px; color: #1E3E62; margin-top: 20px;">Best regards,</p>
-        <p style="font-size: 22px; font-weight: 700; color: #1E3E62;"><strong>Dawid Ciechowski</strong></p>
-        <p style="font-size: 20px; font-weight: 600; color: #1E3E62;">Senior Software Engineer</p>
+        <p style="font-size: 16px; color: #FF6500; margin-top: 20px;">Best regards,</p>
+        <p style="font-size: 22px; font-weight: 700; color: #FF6500;"><strong>Dawid Ciechowski</strong></p>
+        <p style="font-size: 20px; font-weight: 600; color: #FF6500;">Senior Software Engineer</p>
       </div>
     `,
   };
 
   try {
-    await transporter.sendMail(mailToSelfOptions); // Email to yourself
-    await transporter.sendMail(mailToContactingPersonOptions); // Email to the contacting person
+    await transporter.sendMail(mailToSelfOptions);
+    await transporter.sendMail(mailToContactingPersonOptions);
 
     return NextResponse.json({ success: true, message: 'Emails sent successfully' });
   } catch (error) {
