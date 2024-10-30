86d13cce1bdd087385ebb5b44e4a540d8ce72eff
Dawid Ciechowski <dawid.ciechowski@gmail.com>
2024-10-30 18:33:42 +0100

    Some initial formatting for blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -17,17 +17,18 @@ export default async function ArticlePage({ searchParams }: BlogPageProps) {
 
   return (
     <div className="tw-px-[12%] tw-flex tw-flex-col tw-justify-center tw-items-center tw-gap-y-8">
-      <h1>Articles</h1>
-      <ul className="tw-grid tw-grid-cols-2 md:tw-grid-cols-4 tw-gap-x-4 tw-gap-y-6 tw-rounded-xl tw-shadow-glow-md">
+      <h1 className="tw-text-[2.2rem] tw-w-1/2 md:tw-text-[2.5rem] md:tw-w-1/3 lg:tw-w-1/2 lg:tw-text-[3.6rem] tw-font-bold tw-text-main-color tw-border-b-2 tw-rounded-full tw-outline-4 tw-text-center tw-mb-16 tw-mt-8">
+        Beyond The Pages
+      </h1>
+      <ul className="tw-flex tw-flex-col md:tw-grid md:tw-grid-cols-2 lg:tw-grid-cols-4 md:tw-gap-x-4 tw-gap-y-6 ">
         {articles.map((article) => (
           <Link
             key={article._id}
-            className="tw-border-solid tw-border-main-color tw-border-2"
-            href={`/blog/${article._id}`}>
-            <p>
-              {article.title}
-            </p>
-            <p>
+            className="tw-border-solid tw-border-main-color tw-border-2 tw-shadow-glow-tiny tw-rounded-xl tw-text-wrap tw-items-end tw-flex tw-flex-col tw-justify-center"
+            href={`/blog/${article._id}`}
+          >
+            <p className="tw-pb-2 tw-pt-4 tw-pr-2 tw-pl-2">{article.title}</p>
+            <p className="tw-pb-2 tw-pr-2">
               {article.author} - {new Date(article.date).toLocaleDateString()}
             </p>
           </Link>
@@ -46,16 +47,27 @@ function Pagination({
   totalPages: number;
 }) {
   return (
-    <div>
+    <div className="tw-pt-4 tw-pb-16">
       {currentPage > 1 && (
-        <Link href={`/blog?page=${currentPage - 1}`}>Previous</Link>
+        <Link
+          className={`tw-text-[1.3rem] tw-text-black tw-bg-gradient-to-br tw-from-main-color tw-to-[#ac5a23] tw-rounded-md tw-font-semibold tw-px-4 tw-py-1 tw-shadow-glow-sm tw-mr-3`}
+          href={`/blog?page=${currentPage - 1}`}
+        >
+          Previous
+        </Link>
       )}
-      <span>
+      <span className="tw-text-white tw-text-[1.2rem]">
         {" "}
-        Page {currentPage} of {totalPages}{" "}
+        Page <span className="tw-text-main-color">{currentPage}</span> of{" "}
+        <span className="tw-text-text-color">{totalPages}</span>{" "}
       </span>
       {currentPage < totalPages && (
-        <Link href={`/blog?page=${currentPage + 1}`}>Next</Link>
+        <Link
+          className={`tw-pb-16tw-text-[1.3rem] tw-w-[100vw] tw-bg-gradient-to-br tw-from-main-color tw-to-[#ac5a23] tw-text-black tw-rounded-md tw-font-semibold tw-px-4 tw-py-1 tw-shadow-glow-sm tw-ml-3`}
+          href={`/blog?page=${currentPage + 1}`}
+        >
+          Next
+        </Link>
       )}
     </div>
   );
